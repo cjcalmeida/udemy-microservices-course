99c78051b3ee970e716d80d3774a05b710d78e9b
Cristiano Almeida <cicbboyzb@gmail.com>
2019-07-01 03:47:58 -0300

    Add Account Service (mock)

---
+++ udemy-photoapp-account-management/src/main/java/com/cjcalmeida/course/microservice/account/management/PhotoappAccountManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cjcalmeida.course.microservice.account.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappAccountManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoappAccountManagementApplication.class, args);
+	}
+
+}
